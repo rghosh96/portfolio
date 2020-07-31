0863597f37bf1b9f4cae9f0d05a825a21940e8e4
Rashi Ghosh <rashighosh@Rashis-MacBook-Pro.local>
2020-07-31 11:16:39 -0500

    Updates

---
+++ precache-manifest.f0d7ca6e66b887cb09eda59f02b15324.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ece4c25a39d2c118c362c33e318c59",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ddfd67cc289e1a862a4a",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e6518de2e049f2b3bdc5",
+    "url": "/portfolio/static/js/2.267c4beb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.267c4beb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddfd67cc289e1a862a4a",
+    "url": "/portfolio/static/js/main.14e69174.chunk.js"
+  },
+  {
+    "revision": "3d9393fe1099a696fcb7",
+    "url": "/portfolio/static/js/runtime-main.24abe101.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
