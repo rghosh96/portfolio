70a1a9d319fc89868eb41b422935e1df4dcb79eb
Rashi Ghosh <rashighosh@Rashis-MacBook-Pro.local>
2020-08-06 14:01:34 -0500

    better menu

---
+++ src/components/Accordian.js
@@ -0,0 +1,37 @@
+import React, { useState, useRef, useEffect } from 'react'
+
+const Accordion = props => {
+  const [active, setActive] = useState(false)
+  const contentRef = useRef(null)
+
+  useEffect(() => {
+    contentRef.current.style.maxHeight = active ? `${contentRef.current.scrollHeight}px` : '0px'
+  }, [contentRef, active])
+
+  const toogleActive = () => {
+    setActive(!active)
+  }
+
+  const titleStyle = {
+    fontWeight: 600,
+    fontSize: '14px',
+  }
+
+  return (
+    <div className="accordion-section">
+      <button className="accordion-title" onClick={toogleActive}>
+        <p style={titleStyle}>{props.title}</p>
+        {active ? <div className="expand">⊖</div> : <div className="expanded">⊕</div>}
+      </button>
+
+      <div
+        ref={contentRef}
+        className="accordion-content"
+      >
+        {props.children}
+      </div>
+    </div>
+  )
+}
+
+export default Accordion
