dca78a6a23eaec7fe31146bc8343bbd32f7dafe9
Rashi Ghosh <rashighosh@Rashis-MacBook-Pro.local>
2020-08-06 17:43:39 -0500

    Updates

--- precache-manifest.0210c3d953e4c838a5eeea3f5017ccf1.js
+++ precache-manifest.0210c3d953e4c838a5eeea3f5017ccf1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d93bdf5b95435a849cce635c6943de65",
+    "revision": "adc7384bb057a5b3e9f9e0f19c7291d4",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "516ec82d21812e5e08e7",
-    "url": "/portfolio/static/css/main.384a4119.chunk.css"
+    "revision": "a4659c094dec7b6dff33",
+    "url": "/portfolio/static/css/main.b2b7f611.chunk.css"
   },
   {
     "revision": "8b00f14eb039b24c56ed",
@@ -16,19 +16,19 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.382a2b88.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "516ec82d21812e5e08e7",
-    "url": "/portfolio/static/js/main.aae65dfb.chunk.js"
+    "revision": "a4659c094dec7b6dff33",
+    "url": "/portfolio/static/js/main.6f3117eb.chunk.js"
   },
   {
     "revision": "3d9393fe1099a696fcb7",
     "url": "/portfolio/static/js/runtime-main.24abe101.js"
   },
   {
-    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+    "revision": "90efd8d6bcc447fb6a46553af85918b3",
+    "url": "/portfolio/static/media/flowers.90efd8d6.png"
   },
   {
-    "revision": "34c9738ceda2d2e7595d5dc8cdb19eb3",
-    "url": "/portfolio/static/media/pictur.34c9738c.png"
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b1085208424a337e77acd502db4fd173.js"
+  "/portfolio/precache-manifest.0210c3d953e4c838a5eeea3f5017ccf1.js"
 );
 
 self.addEventListener('message', (event) => {
