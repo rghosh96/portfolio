e28a072bbd9f53503a112bab96c33b811bb44ad4
Rashi Ghosh <rashighosh@Rashis-MacBook-Pro.local>
2020-08-06 18:09:00 -0500

    make pretty

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,40 @@
 import React, {useState, useEffect, createRef} from 'react';
 import '../css/home.css';
 import Accordion from './Accordian.js'
-import pic from './pictur.png';
+import pic from './flowers.png';
 
 export default function Home(props) {
     return (
+      
       <div className="home">
         <img src={pic} alt="Flowers in Chania"/>
+        <h1> rashi ghosh</h1>
+        <hr></hr>
+        <div className="embed">
         <div className="menu">
-          <h1> rashi ghosh</h1>
+        
+          
           <p>deep seated fear of failure, broad political understandings, airs on the side of caution, 
             a few complexes, blended Indian culture with American exceptionalist ideas</p>
 
           <Accordion title="story">
-        <span className="accordion-text">coming soon ! :)</span>
+        <span className="accordion-text">coming soon ! :) deep seated fear of failure, broad political understandings, airs on the side of caution, 
+            a few complexes, blended Indian culture with American exceptionalist ideas</span>
       </Accordion>
       <Accordion title="ingredients">
-        <span className="accordion-text">coming soon! :)</span>
+        <span className="accordion-text">coming soon! :) deep seated fear of failure, broad political understandings, airs on the side of caution, 
+            a few complexes, blended Indian culture with American exceptionalist ideas</span>
       </Accordion>
       <Accordion title="nutrition facts">
-        <span className="accordion-text">coming soon! :)</span>
+        <span className="accordion-text">coming soon! :) deep seated fear of failure, broad political understandings, airs on the side of caution, 
+            a few complexes, blended Indian culture with American exceptionalist ideas</span>
       </Accordion>
       <Accordion title="contact">
-        <span className="accordion-text">coming soon! :)</span>
+        <span className="accordion-text">coming soon! :) deep seated fear of failure, broad political understandings, airs on the side of caution, 
+            a few complexes, blended Indian culture with American exceptionalist ideas</span>
       </Accordion>
-
-
-        </div>
       </div>
+      </div>
+        </div>
     )
   }
