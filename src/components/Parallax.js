604d9b8c4d66eff5419d3eb6549d86f7989e67bc
Rashi Ghosh <rashighosh96@gmail.com>
2021-11-13 18:04:40 -0500

    trying to add in scroll and nav

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -1,8 +1,13 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import '../css/about.css';
 import '../css/master.css';
 import { withTheme } from 'styled-components'
+import { faUserCircle, faCode, faEnvelope, faBookOpen } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { motion } from 'framer-motion'
+import { Link } from 'react-scroll'
+
 import MainPage from './Main';
 import AboutSection from './About';
 import ResearchPage from './Research';
@@ -11,11 +16,77 @@ import ContactPage from './Contact';
 
 const ParallaxTest = (props) => {
   const parallax = useRef()
+  const [showNav, setShowNav] = useState(false);
   const url = (name: string, wrap = false) =>
   `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
   console.log(url('stars', true))
+
+  useEffect(() => {
+    function handleScroll() {
+      console.log("scrolling")
+      const yPos = window.scrollY;
+      const windowHeight = window.innerHeight - (window.innerHeight/2);
+      console.log(windowHeight)
+      console.log(yPos)
+      yPos > windowHeight ? setShowNav(true) : setShowNav(false)
+    }
+    window.addEventListener('scroll', handleScroll, false);
+    return () => {
+      window.removeEventListener('scroll', handleScroll, false);
+    }
+  })
+
   return (
     <div class="parallax">
+        <motion.div animate={{opacity: showNav? 1 : 0}} initial={{opacity: 0}}>
+      <div className="navigation">
+
+<Link to="sectionAbout" className="navItem" smooth={true} duration={1000} >
+  <FontAwesomeIcon
+    color={props.theme.accent}
+    icon={faUserCircle}
+    size='1x'
+  />
+  <p className="navLink">About</p>
+
+
+</Link>
+
+<Link className="navItem" to="sectionProjects" smooth={true} duration={1000} >
+<FontAwesomeIcon
+    color={props.theme.accent}
+    icon={faCode}
+    size='1x'
+  />
+<p className="navLink">Projects</p>
+
+
+</Link>
+
+<Link className="navItem" to="sectionResearch" smooth={true} duration={1000} >
+<FontAwesomeIcon
+    color={props.theme.accent}
+    icon={faBookOpen}
+    size='1x'
+  />
+<p className="navLink">Research</p>
+
+
+</Link>
+
+<Link className="navItem" to="sectionContact" smooth={true} duration={1000} >
+<FontAwesomeIcon
+    color={props.theme.accent}
+    icon={faEnvelope}
+    size='1x'
+  />
+<p className="navLink">Contact</p>
+
+
+</Link>
+
+</div>
+        </motion.div>
       <Parallax ref={parallax} pages={5}>
         <ParallaxLayer offset={1} speed={1}  />
         <ParallaxLayer offset={2} speed={1}  />
@@ -25,7 +96,17 @@ const ParallaxTest = (props) => {
         <ParallaxLayer
           offset={0}
           speed={0}
-          factor={3}
+          factor={5}
+          style={{
+            backgroundImage: url('stars', true),
+            backgroundSize: 'cover',
+          }}
+        />
+
+        <ParallaxLayer
+          offset={.5}
+          speed={.3}
+          factor={5}
           style={{
             backgroundImage: url('stars', true),
             backgroundSize: 'cover',
