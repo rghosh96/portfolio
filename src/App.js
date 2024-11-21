690b25a5572c0dc0b88cb9b2f406470508b41a1c
rghosh96 <rashighosh96@gmail.com>
2024-11-21 12:08:37 -0500

    starting to build out

--- src/App.js
+++ src/App.js
@@ -1,53 +1,25 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import HomePage from './components/Home'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme } from './styles/themes';
-import { GlobalStyles } from './styles/global';
-import './css/master.css';
-import ToggleSwitch from './components/Toggle';
-import ResumePage from './components/Resume';
-import ParallaxPage from './components/Parallax';
-import AllProjectsPage from './components/AllProjects';
+import logo from './logo.svg';
+import './App.css';
+import Spline from '@splinetool/react-spline';
 
-function App(props) {
-  // check if persisting theme exists, set to last theme; else light is default
-  let siteTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'dark'
-  const [theme, setTheme] = useState(siteTheme);
-
-// The function that toggles between themes
-const toggleTheme = () => {
-  // if the theme is not light, then set it to dark
-  if (theme === 'light') {
-    setTheme('dark');
-    // save this choice to local storage
-    localStorage.setItem('theme', 'dark');
-  // otherwise, it should be light
-  } else {
-    setTheme('light');
-    // save this choice to local storage
-    localStorage.setItem('theme', 'light');
-  }
-}
-
-
+function App() {
   return (
-    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      <div className="leContainer">
-      <ToggleSwitch theme={theme === 'light' ? lightTheme : darkTheme} toggleTheme={toggleTheme}>Toggle theme</ToggleSwitch>
-      <GlobalStyles />
-      <BrowserRouter>
-      <Switch>
-        <Route exact path="/oldHomePage" component={HomePage} />
-        <Route exact path="/resume" component={ResumePage} />
-        <Route exact path="/portfolio" component={ParallaxPage} />
-        <Route exact path="/allprojects" component={AllProjectsPage} />
-      </Switch>
-      </BrowserRouter>
-      </div>
-    </ThemeProvider>
-    
+    <div className="App">
+      <Spline className="spline" scene="https://prod.spline.design/sxHMKuNon-DYgy7M/scene.splinecode" />
+    <div className="content">
+       <div className="title">
+          <h1>rashi ghosh</h1>
+          <h3>phd student | university of florida</h3>
+          <div className="menu">
+            <button>research</button>
+            <button>projects</button>
+            <button>contact</button>
+            <button>cv</button>
+          </div> 
+        </div>
+      </div>    
+          <div class="footer"></div>
+    </div>
   );
 }
 
